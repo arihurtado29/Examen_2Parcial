7d5103b7f15cbbfac963cda4bf372363d4029818
arihurtado29 <ahurtado5@ucol.mx>
2025-04-09 12:55:23 -0600

    Agregar archivos de proyecto.

---
+++ Examen_2Parcial/Components/Data/Productos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Examen_2Parcial.Components.Data
+{
+    public class Productos
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "El número de inventario debe ser un número positivo mayor a 0.")]
+        public int Inventario { get; set; }
+        [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+        [MaxLength(200, ErrorMessage = "El nombre no puede exceder los 200 caracteres.")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar el departamento")]
+        [RegularExpression("^(Frutas|Verduras|Lácteos)$", ErrorMessage = "El departamento debe ser Frutas, Verduras o Lácteos.")]
+        public string? Departamento { get; set; }
+    }
+}
